530fa7e8bf1af3c33cc960f71746e9130639ca4e
AiswaryaMukherjee <aiswarya.mukherjeee@fau.de>
2026-02-13 17:20:30 +0100

    FPU Software Files Added

---
+++ software/fpu_test/func.h
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+#ifndef fputetst_H
+#define fputetst_H
+
+float mul(float in1, float in2);
+void reverse(char* str, int len);
+int int_to_str(int32_t x, char str[], int d);  
+void fp2str(float value, char *str, int precision);
+
+#endif
